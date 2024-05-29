aa0a17ba924d7f23a239fc7ac64916b1f90f33a6
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2024-05-29 13:48:38 -0500

    added docstring specifications for the functions eliminated the pretty-print function for now

--- python/jstone/jstone.py
+++ python/jstone/jstone.py
@@ -1,12 +1,25 @@
-def validate_jstone():
+def validate_jstone(text):
+  """Determines if the text is valid jstone or not"""
   pass
 
-def cannonicalize_jstone():
+def cannonicalize_jstone(text, hasher, return_all=false):
+  """Determines the cannonical form for the jstone text and returns it.
+
+  arguments:
+  text -- the JSTONE text to be cannonicalized.
+  hasher -- a hash function that returns a binary hash of a binary input.
+
+  keyword arguments:
+  return_all -- If True, this function returns a dictionary where the values are every cannonicalized component of the text document and the keys are their associated hashes.
+  """
   pass
 
-def expand_jstone():
-  pass
+def expand_jstone(text, resolver):
+  """Eliminates all references to other JSTONE documents within the JSTONE document text and returns the new document. 
 
-def pretty_jstone():
+  arguments:
+  text -- the JSTONE text to be expanded.
+  resolver -- a function that takes a binary hash and returns the binary data associated with it. 
+  """
   pass
 
